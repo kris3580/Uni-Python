4a19e84aee0ecb49dad8ddc657fecbcde0f85c61
kris3580 <145036874+kris3580@users.noreply.github.com>
2025-02-19 13:45:19 +0200

    lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,31 @@
+# SARCINA 1
+# 3
+print("Type your name:")
+username = input()
+print("Hi " + username + "!")
+
+# 4
+f = 3.14
+i = 1
+s1 = "Hey"
+s2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+# 5
+print(type(f))
+print(type(i))
+
+# 6
+print(len(s1))
+
+# 7
+s2 = s2.upper()
+print(s2 +'\n')
+
+# 8
+print(s2[0:11])
+
+# 10
+template1 = "{} is a nice color."
+print(template1.format("Green"))
+
+print(f"Pi is aproximately {f}.")
